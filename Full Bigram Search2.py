4bbfb09b5add0240092cb797554097a8b9370b56
znhoughton <77252741+znhoughton@users.noreply.github.com>
2021-04-22 13:05:39 -0400

    Update Full Bigram Search2.py

--- Full Bigram Search2.py
+++ Full Bigram Search2.py
@@ -9,11 +9,7 @@ if __name__ == '__main__':
     from zs import ZS
     import math
     import csv
-    from threading import Thread
-    import time
-
-    class TimeoutError(Exception):
-        pass
+   
 
     
     z = ZS("D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\google-books-eng-us-all-20120701-2gram.zs")
@@ -21,123 +17,25 @@ if __name__ == '__main__':
 
     corpus_size = 0
     for item in z_1gram.search():
-        corpus_size += 1
+       corpus_size += 1
     
-    
-    ##Volume count is probably better to use in order to avoid technical books inflating the number of otherwise infrequent words
-    
-
-    
-    def two_gram_odds_ratio(word1, word2, word1POS, word2POS):
-        
-        if __name__ == '__main__':
-         
-            word1 = word1.lower()
-            word2 = word2.lower()
-            word1_capital = word1.capitalize() #search query doesn't let us ignore case, so we have to account for each combination of capital and lowercase in English
-            word2_capital = word2.capitalize()
-            word1 = word1.encode("utf-8")
-            word2 = word2.encode("utf-8")
-            word1_capital = word1_capital.encode("utf-8")
-            word2_capital = word2_capital.encode("utf-8")
-            two_gram_one = 0
-            two_gram_two = 0
-            for record in z.search(start=word1 + b" " + word2 + b"\t1980", stop=word1 + b" " + word2 + b"\t2009"): #search for each 2gram between 1980 and 2009
-                navy_ship = record.decode("utf-8")
-                n_gram = navy_ship.split("\t")#separate the string so we can focus on what we care about 
-                two_gram_one = two_gram_one + int(n_gram[2]) #column 3 gives us "match count"
-                two_gram_two = two_gram_two + int(n_gram[3]) #column 4 gives us "volume count"
-            for record in z.search(start=word1_capital + b" " + word2_capital + b"\t1980", stop=word1_capital + b" " + word2_capital + b"\t2009"): #accounting for capitalization
-                navy_ship = record.decode("utf-8")
-                n_gram = navy_ship.split("\t")   
-                two_gram_one = two_gram_one + int(n_gram[2])
-                two_gram_two = two_gram_two + int(n_gram[3])
-            for record in z.search(start=word1_capital + b" " + word2 + b"\t1980", stop=word1_capital + b" " + word2 + b"\t2009"): #accounting for capitalization
-                navy_ship = record.decode("utf-8")
-                n_gram = navy_ship.split("\t")   
-                two_gram_one = two_gram_one + int(n_gram[2])
-                two_gram_two = two_gram_two + int(n_gram[3])
-            for record in z.search(start=word1 + b" " + word2_capital + b"\t1980", stop=word1 + b" " + word2_capital + b"\t2009"): #accounting for capitalization
-                navy_ship = record.decode("utf-8")
-                n_gram = navy_ship.split("\t")   
-                two_gram_one = two_gram_one + int(n_gram[2])
-                two_gram_two = two_gram_two + int(n_gram[3])
-        
-            one_gram_one = 0
-            one_gram_two = 0
-            for record in z_1gram.search(prefix=word1): #number of times word1 appears total
-                navy = record.decode("utf-8")
-                one_gram = navy.split("\t")
-                if int(one_gram[1]) >= 1980:
-                    one_gram_one = one_gram_one +int(one_gram[2]) #match count
-                    one_gram_two = one_gram_two +int(one_gram[3]) #volume count
-            for record in z_1gram.search(prefix=word1_capital):  #need to account for capitalization for this as well
-                navy = record.decode("utf-8")
-                one_gram = navy.split("\t")
-                if int(one_gram[1]) >= 1980:
-                    one_gram_one = one_gram_one +int(one_gram[2]) #match count
-                    one_gram_two = one_gram_two +int(one_gram[3]) #volume count
-
-            n2_count_one = 0
-            n2_count_two = 0
-            for record in z_1gram.search(prefix=word2): #number of times word1 appears total
-                ship = record.decode("utf-8")
-                n2_gram = ship.split("\t")
-                if int(n2_gram[1]) >= 1980:
-                    n2_count_one = n2_count_one +int(n2_gram[2]) #match count
-                    n2_count_two = n2_count_two +int(n2_gram[3]) #volume count
-            for record in z_1gram.search(prefix=word2_capital):  #need to account for capitalization for this as well
-                ship = record.decode("utf-8")
-                n2_gram = ship.split("\t")
-                if int(n2_gram[1]) >= 1980:
-                    n2_count_one = n2_count_one +int(n2_gram[2]) #match count
-                    n2_count_two = n2_count_two +int(n2_gram[3]) #volume count
-
-            
-            #count((count(gun rights)/count(gun)) / count(gun) - count(gun rights) / count(gun) --> this is the odds ratio
-        
-            if one_gram_one != 0 and one_gram_two != 0:
-                P_value = (two_gram_one / one_gram_one)
-                oddsratio_col1 = (two_gram_one / one_gram_one) / ((one_gram_one - two_gram_one) / one_gram_one)
-                oddsratio_col2 = (two_gram_two / one_gram_two) / ((one_gram_two - two_gram_two) / one_gram_two)
-                deltaP_col1 = (two_gram_one / one_gram_one) - ((n2_count_one - two_gram_one) / (corpus_size - one_gram_one))
-                deltaP_col2 = (two_gram_two / one_gram_two) - ((n2_count_two - n2_count_two) / (corpus_size - one_gram_two))
-                oddsratio_simplified1 = (two_gram_one / one_gram_one)
-                oddsratio_simplified2 = (two_gram_two / one_gram_two)
-                oddsratio_deltap_difference1 = oddsratio_col1 = deltaP_col1 #this is included so we can find words with the maximum difference between deltaP and oddsratio
-                oddsratio_deltap_difference2 = oddsratio_col2 = deltaP_col2
-            
-        #this allows us to get an idea of how predictive word1 is of word 2
-        #in other words, what are the odds of word2 given word1? p(word2|word1)
-        
-        
-            if two_gram_one != 0 and one_gram_one != 0: #this prevents an error due to trying to divide by 0 and doesn't bother returning values for a bigram with no entries
-                return(word1.decode('utf-8'), word2.decode('utf-8'), word1POS, word2POS, two_gram_one, two_gram_two, one_gram_one, one_gram_two, P_value, oddsratio_col1, oddsratio_col2, deltaP_col1, deltaP_col2, oddsratio_deltap_difference1, oddsratio_deltap_difference2, n2_count_one, n2_count_two, corpus_size)
-            else: 
-                return(word1.decode('utf-8'), word2.decode('utf-8'), word1POS, word2POS, two_gram_one, two_gram_two, one_gram_one, one_gram_two, "NA", "NA", "NA", "NA", "NA", "NA", "NA", n2_count_one, n2_count_two, corpus_size)
-
-            if stop_event.is_set():
-                sys.exit()
-
-    action_thread = Thread(target=two_gram_odds_ratio())
-    action_thread.start()
-    action_thread.join(timeout=420)
-    stop_event.set()
-    print("Connection Timedout")
 
     if __name__ == '__main__':
         with open('D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\csvtest.csv', newline='') as csvfile_input:
             with open('D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\Full Bigram2.csv', 'w') as csvfile_output:
-                my_two_grams_reader = csv.reader(csvfile_input, delimiter=',')
-                my_two_grams_writer = csv.writer(csvfile_output, delimiter=',', lineterminator='\n')
-                my_two_grams_writer.writerow(["N1", "N2", "N1POS", "N2POS", "Match Count of 2gram", "Volume Count of 2gram", "Match Count of N1", "Volume Count of N1", "P Value", "Odds Ratio of Match count", "Odds Ratio of Volume Count", "Delta P of Match Count", "Delta P of Volume Count", "Difference Between Odds Ratio and DeltaP Match Count", "Difference Between Odds Ratio and DeltaP Volume Count", "N2 Match Count", "N2 Volume Count", "Corpus Size"])
+                bigram_writer = csv.writer(csvfile_output, delimiter=',', lineterminator='\n')
+                bigram_writer.writerow(["N1", "N2", "N1POS", "N2POS", "N1 Match Count", "N2 Volume Count", "N2 Match Count", "N2 Volume Count", "Odds Ratio of Match count", "Odds Ratio of Volume Count", "Delta P of Match Count", "Delta P of Volume Count", "Difference Between Odds Ratio and DeltaP Match Count", "Difference Between Odds Ratio and DeltaP Volume Count", "Corpus Size"])
                 
-                memory = []
+                memory = {}
                 for i, item in enumerate (z.search()):
                     decoded_string = item.decode('utf-8')
                     output = decoded_string.split('\t')
+                    match_count = output[2]
+                    volume_count = output[3]
+                    year = output[1]
                     bigram = output[0].split(' ')
                     if i % 10000000 == 0:
+                        print(decoded_string)
                         print(bigram)
                     
                     if len(bigram[0].split('_')) == 2:
@@ -150,9 +48,69 @@ if __name__ == '__main__':
                     else:
                         word2 = bigram[1]
                         word2POS = None
+                    word1 = word1.lower()
+                    word2 = word2.lower()
 
-                    if (word1, word2) not in memory:
-                        memory.append((word1, word2))                    
-                        if word1.isalpha() and word2.isalpha():
-                            odds_ratio = two_gram_odds_ratio(word1, word2, word1POS, word2POS)
-                            my_two_grams_writer.writerow(odds_ratio)
+                    if word1.isalpha() and word2.isalpha():
+                        if (word1, word2) in memory:
+                            old_output2, old_output3, old_POS1, old_POS2, old_one_gram2, old_one_gram3, old_n2_count2, old_n2_count3 = memory[(word1, word2)]
+                            memory[(word1, word2)] = (old_output2 + output[2], old_output3 + output[3], word1POS, word2POS, old_one_gram2, old_one_gram3, old_n2_count2, old_n2_count3)
+                        
+                        else:
+                            n1_match = 0
+                            n1_volume = 0
+
+                            word1_capital = word1.capitalize() #search query doesn't let us ignore case, so we have to account for each combination of capital and lowercase in English
+                            word2_capital = word2.capitalize()
+                            
+                            word1_encoded = word1.encode('utf-8')
+                            word2_encoded = word2.encode('utf-8')
+                            word1_capital = word1_capital.encode('utf-8')
+                            word2_capital = word2_capital.encode('utf-8')
+
+                            for record in z_1gram.search(prefix=word1_encoded): #number of times word1 appears total
+                                navy = record.decode("utf-8")
+                                one_gram = navy.split("\t")
+                                if int(one_gram[1]) >= 1980:
+                                    n1_match = n1_match +int(one_gram[2]) #match count
+                                    n1_volume = n1_volume +int(one_gram[3]) #volume count
+                            for record in z_1gram.search(prefix=word1_capital):  #need to account for capitalization for this as well
+                                navy = record.decode("utf-8")
+                                one_gram = navy.split("\t")
+                                if int(one_gram[1]) >= 1980:
+                                    n1_match = n1_match +int(one_gram[2]) #match count
+                                    n1_volume = n1_volume +int(one_gram[3]) #volume count
+
+                            n2_count_one = 0
+                            n2_count_two = 0
+                            for record in z_1gram.search(prefix=word2_encoded): #number of times word1 appears total
+                                ship = record.decode("utf-8")
+                                n2_gram = ship.split("\t")
+                                if int(n2_gram[1]) >= 1980:
+                                    n2_count_one = n2_count_one +int(n2_gram[2]) #match count
+                                    n2_count_two = n2_count_two +int(n2_gram[3]) #volume count
+                            for record in z_1gram.search(prefix=word2_capital):  #need to account for capitalization for this as well
+                                ship = record.decode("utf-8")
+                                n2_gram = ship.split("\t")
+                                if int(n2_gram[1]) >= 1980:
+                                    n2_count_one = n2_count_one +int(n2_gram[2]) #match count
+                                    n2_count_two = n2_count_two +int(n2_gram[3]) #volume count
+
+                            memory[(word1, word2)] = (output[2], output[3], word1POS, word2POS, n1_match, n1_volume, n2_count_two, n2_count_two) 
+                        
+                                      
+                for word1, word2 in memory:
+                    
+                    bigram_match, bigram_volume, N1_match, N1_volume, N2_match, N2_volume = memory[(word1, word2)]
+                    
+                    odds_ratio_match = (bigram_match) / (N1_match - bigram_match)
+                    odds_ratio_volume = (bigram_volume) / (N1_volume - bigram_volume)
+                    deltap_match = (bigram_match / N1_match) - ((N2_match - bigram) / (corpus_size - N1_match))
+                    deltap_volume = (bigram_volume / N1_volume) - ((N2_volume - bigram) / (corpus_size - N1_volume))
+                    oddsratio_deltap_difference1 = abs(odds_ratio_match - deltap_match)
+                    oddsratio_deltap_difference2 = abs(odds_ratio_volume - deltap_volume)
+
+                    bigram_writer.writerow(word1, word2, word1POS, word2POS, N1_match, N1_volume, N2_match, N2_volume, bigram_match, bigram_volume, odds_artio_match, odds_ratio_volume, deltap_match, deltap_volume, oddsratio_deltap_difference1, oddsratio_deltap_difference2, corpus_size)
+                
+               
+                
