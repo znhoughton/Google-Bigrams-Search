b24e47ce111ff00bd646a8a7bada3653ae6d127c
znhoughton <77252741+znhoughton@users.noreply.github.com>
2021-04-27 16:27:44 -0400

    Update 2Gram Odds Ratio Write CSV (POS unspecified).py

--- 2Gram Odds Ratio Write CSV (POS unspecified).py
+++ 2Gram Odds Ratio Write CSV (POS unspecified).py
@@ -15,12 +15,28 @@ if __name__ == '__main__':
 
     corpus_size = 0
     for item in z_1gram.search():
-        corpus_size += 1
+        one_gram_string = item.decode('utf-8')
+        one_gram_output = one_gram_string.split('\t')
+        one_gram_match_count = one_gram_output[2]
+        one_gram_volume_count = one_gram_output[3]
+        word = one_gram_output[0]
+        word = word.lower()
 
-    def two_gram_odds_ratio(word1, word2):
+        if len(word.split('_')) == 2:
+            word, POS = word.split('_')
+
+        else:
+            word = word
+        
+        if word.isalpha():
+            corpus_size = corpus_size + int(one_gram_match_count)
+    
+    
+    def two_gram_odds_ratio(word1, word2, word1POS, word2POS):
         
         if __name__ == '__main__':
-        
+            word1 = word1.lower()
+            word2 = word2.lower()
             word1_capital = word1.capitalize() #search query doesn't let us ignore case, so we have to account for each combination of capital and lowercase in English
             word2_capital = word2.capitalize()
             word1 = word1.encode("utf-8")
@@ -83,7 +99,7 @@ if __name__ == '__main__':
             
             #count((count(gun rights)/count(gun)) / count(gun) - count(gun rights) / count(gun) --> this is the odds ratio
         
-            if one_gram_one != 0:
+            if one_gram_one != 0 and one_gram_two != 0:
                 P_value = (two_gram_one / one_gram_one)
                 oddsratio_col1 = (two_gram_one / one_gram_one) / ((one_gram_one - two_gram_one) / one_gram_one)
                 oddsratio_col2 = (two_gram_two / one_gram_two) / ((one_gram_two - two_gram_two) / one_gram_two)
@@ -91,26 +107,43 @@ if __name__ == '__main__':
                 deltaP_col2 = (two_gram_two / one_gram_two) - ((n2_count_two - n2_count_two) / (corpus_size - one_gram_two))
                 oddsratio_simplified1 = (two_gram_one / one_gram_one)
                 oddsratio_simplified2 = (two_gram_two / one_gram_two)
+                oddsratio_deltap_difference1 = oddsratio_col1 = deltaP_col1 #this is included so we can find words with the maximum difference between deltaP and oddsratio
+                oddsratio_deltap_difference2 = oddsratio_col2 = deltaP_col2
             
         #this allows us to get an idea of how predictive word1 is of word 2
-        #in other words, what are the odds of word2 given word1?
+        #in other words, what are the odds of word2 given word1? p(word2|word1)
         
         
-            if two_gram_one != 0 and one_gram_one != 0: #this prevents an error due to trying to divide by 0
-                return(word1.decode('utf-8'), word2.decode('utf-8'), two_gram_one, two_gram_two, one_gram_one, one_gram_two, P_value, oddsratio_col1, oddsratio_col2, deltaP_col1, deltaP_col2, oddsratio_simplified1, oddsratio_simplified2, n2_count_one, n2_count_two, corpus_size)
+            if two_gram_one != 0 and one_gram_one != 0: #this prevents an error due to trying to divide by 0 and doesn't bother returning values for a bigram with no entries
+                return(word1.decode('utf-8'), word2.decode('utf-8'), word1POS, word2POS, two_gram_one, two_gram_two, one_gram_one, one_gram_two, P_value, oddsratio_col1, oddsratio_col2, deltaP_col1, deltaP_col2, oddsratio_deltap_difference1, oddsratio_deltap_difference2, n2_count_one, n2_count_two, corpus_size)
             else: 
-                return(word1.decode('utf-8'), word2.decode('utf-8'), two_gram_one, two_gram_two, one_gram_one, one_gram_two, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA")
+                return(word1.decode('utf-8'), word2.decode('utf-8'), word1POS, word2POS, two_gram_one, two_gram_two, one_gram_one, one_gram_two, "NA", "NA", "NA", "NA", "NA", "NA", "NA", n2_count_one, n2_count_two, corpus_size)
 
     if __name__ == '__main__':
         with open('D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\csvtest.csv', newline='') as csvfile_input:
-            with open('D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\Key.csv', 'w') as csvfile_output:
+            with open('D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\Full Bigram.csv', 'w') as csvfile_output:
                 my_two_grams_reader = csv.reader(csvfile_input, delimiter=',')
                 my_two_grams_writer = csv.writer(csvfile_output, delimiter=',', lineterminator='\n')
-                my_two_grams_writer.writerow(["N1", "N2", "Match Count of 2gram", "Volume Count of 2gram", "Match Count of N1", "Volume Count of N1", "P Value", "Odds Ratio of Match count", "Odds Ratio of Volume Count", "Delta P of Match Count", "Delta P of Volume Count", "Odds ratio simplified (match count)", "Odds ratio simplified (volume count)", "N2 Match Count", "N2 Volume Count", "Corpus Size"])
-                for row in my_two_grams_reader:
-                    odds_ratio = two_gram_odds_ratio(row[0], row[1])
-                    my_two_grams_writer.writerow(odds_ratio)
-                    #header should be:
-                    #word1, word2, match count of 2gram, volume count of 2gram, match count of N1, volume count of N1, P value, odds ratio of match count, odds ratio of volume count, log odds ratio match count, log odds ratio volume count
-            
-
+                my_two_grams_writer.writerow(["N1", "N2", "N1POS", "N2POS", "Match Count of 2gram", "Volume Count of 2gram", "Match Count of N1", "Volume Count of N1", "P Value", "Odds Ratio of Match count", "Odds Ratio of Volume Count", "Delta P of Match Count", "Delta P of Volume Count", "Difference Between Odds Ratio and DeltaP Match Count", "Difference Between Odds Ratio and DeltaP Volume Count", "N2 Match Count", "N2 Volume Count", "Corpus Size"])
+                
+                for i, item in enumerate (z.search()):
+                    decoded_string = item.decode('utf-8')
+                    output = decoded_string.split('\t')
+                    bigram = output[0].split(' ')
+                    if i % 10000000 == 0:
+                        print(bigram)
+                    
+                    if len(bigram[0].split('_')) == 2:
+                        word1, word1POS = bigram[0].split('_')
+                    else:
+                        word1 = bigram[0]
+                        word1POS = None
+                    if len(bigram[1].split('_')) == 2:
+                        word2, word2POS = bigram[1].split('_')
+                    else:
+                        word2 = bigram[1]
+                        word2POS = None
+                
+                    if word1.isalpha() and word2.isalpha():
+                        odds_ratio = two_gram_odds_ratio(word1, word2, word1POS, word2POS)
+                        my_two_grams_writer.writerow(odds_ratio)
